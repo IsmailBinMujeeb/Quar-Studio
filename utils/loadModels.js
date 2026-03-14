15bd5a4b6cdcf24b0e60ee629620c7344766216d
ismailbinmujeeb <ismailbinmujeeb@gmail.com>
2026-03-14 18:27:07 +0530

    feat: add typescript support

--- utils/loadModels.js
+++ utils/loadModels.js
@@ -5,23 +5,26 @@ import chalk from "chalk";
 import { pathToFileURL } from "url";
 
 export default (modelPath) => {
-    if (!fs.existsSync(modelPath)) {
-        console.log(
-            chalk.red.bold('[ERROR]') +
-            ' The specified model path does not exist:\n  ' +
-            chalk.gray(modelPath)
-        );
-        console.log('Tip: Check the path or create the folder first.');
-        process.exit(1);
+  if (!fs.existsSync(modelPath)) {
+    console.log(
+      chalk.red.bold("[ERROR]") +
+        " The specified model path does not exist:\n  " +
+        chalk.gray(modelPath)
+    );
+    console.log("Tip: Check the path or create the folder first.");
+    process.exit(1);
+  }
+
+  fs.readdirSync(modelPath).forEach(async (file) => {
+    if (file.endsWith(".js") || file.endsWith(".ts")) {
+      const filePath = path.join(modelPath, file);
+      const moduleUrl = pathToFileURL(filePath);
+      await import(moduleUrl.href);
     }
+  });
 
-    fs.readdirSync(modelPath).forEach(async (file) => {
-        if (file.endsWith('.js')) {
-            const filePath = path.join(modelPath, file);
-            const moduleUrl = pathToFileURL(filePath);
-            await import(moduleUrl.href);
-        }
-    });
-
-    console.log(chalk.blue.bold('[INFO]'), chalk.gray("Models loaded from: " + modelPath))
-}
+  console.log(
+    chalk.blue.bold("[INFO]"),
+    chalk.gray("Models loaded from: " + modelPath)
+  );
+};
